9fcac51f4241893b2a87d4f69777705bc87c415d
Home <Home@DESKTOP-U9GJ8JH>
2017-03-04 14:57:45 +0530

    test

---
+++ DIGICOMSolution/DIGICOM.Web/wwwroot/JS/Site.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    var $sidebarwrapper = $("#sidebar, #wrapper");
+    $("#sidebarToggle").on('click', function () {
+        $sidebarwrapper.toggleClass('hide-sidebar');
+    });
+    //var ee = $('#username');
+    //ee.text("test2");
+    //var main = $('#main');
+    //main.on('mouseenter', function () {
+    //    main.style = " background-color:#888;";
+    //});
+    //main.on('mouseleave', function () {
+    //    main.style = "";
+    //});
+    //var memuItem = $("ul.menu li a");
+    //memuItem.on("click", function() {
+    //    alert($(this).text());
+    //})
+})();
